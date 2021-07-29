4ba58d3d967b1330825a61fb46ec593e28310abf
SzczeZam <wookslandline@protonmail.com>
2021-07-29 03:48:48 -0400

    added StatGraph element

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,8 @@
-export const CapitalCase = (string) =>
-[...string].map((char, i) => {
+export const CapitalCase = (string) =>{
+  const convertedString = [...string].map((char, i) => {
   const isLetter = /^[a-zA-Z]+$/.test(string[i - 1]);
   const parsedChar = !isLetter || i === 0 ? char.toUpperCase() : char;
-  return parsedChar;
-});
+  return parsedChar
+})
+  return convertedString.join("")
+};
