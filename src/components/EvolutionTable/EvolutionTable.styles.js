431e9ad780511f236eaa186551aeb94b383a3f69
Stringmander <nrusso@protonmail.com>
2021-08-19 14:09:15 -0400

    fix: PokeDetail styling

--- src/components/EvolutionTable/EvolutionTable.styles.js
+++ src/components/EvolutionTable/EvolutionTable.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { DoubleArrow } from "@material-ui/icons";
+import { Card } from "@material-ui/core";
 
 export const EvoTable = styled.div`
   display: flex;
@@ -7,8 +8,14 @@ export const EvoTable = styled.div`
   margin: 5px;
 `;
 
-export const EvoCard = styled.div`
-  padding: 5px;
+export const EvoCard = styled(Card)`
+  border-radius: 5px;
+  & p {
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    color: white;
+    background-color: black;
+  }
 `;
 
 export const EvoArrow = styled(DoubleArrow)`
