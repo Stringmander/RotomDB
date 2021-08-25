3bc38b924be2135a6ad455586a10218e7e5238fa
Stringmander <nrusso@protonmail.com>
2021-08-25 19:51:48 -0400

    fix: clean up MovesTable story

--- src/components/MovesTable/MovesTable.stories.js
+++ src/components/MovesTable/MovesTable.stories.js
@@ -1,51 +1,12 @@
 import { React } from "react";
 
 import MovesTable from ".";
-import { mockBulbasaur, mockCharmander, pokeTypeArg } from "../../stories";
+import { mockBulbasaur, mockCharmander } from "../../stories";
 
 export default {
   title: "Design System/MovesTable",
   component: MovesTable,
-  argTypes: {
-    moves: {
-      controls: "object",
-    },
-    types: {
-      controls: "object",
-    },
-    // typeX: pokeTypeArg,
-    // typeY: pokeTypeArg,
-  },
 };
-
-// export const SingleTypePokemon = ({ moves, typeX }) => {
-//   return <MovesTable moves={moves} pokeType={[typeX]} />;
-// };
-
-// SingleTypePokemon.argTypes = {
-//   typeY: {
-//     table: {
-//       disable: true,
-//     },
-//   },
-// };
-
-// SingleTypePokemon.args = {
-//   moves: mockCharmander.moves,
-//   typeX: "Fire",
-// };
-
-// export const DualTypePokemon = ({ moves, typeX, typeY }) => {
-//   const types = [typeX, typeY];
-
-//   return <MovesTable moves={moves} pokeType={types} />;
-// };
-
-// DualTypePokemon.args = {
-//   moves: mockBulbasaur.moves,
-//   typeX: "Grass",
-//   typeY: "Poison",
-// };
 
 const MovesTableStory = (args) => <MovesTable {...args} />;
 
@@ -53,4 +14,10 @@ export const Bulbasaur = MovesTableStory.bind({});
 Bulbasaur.args = {
   moves: mockBulbasaur.moves,
   pokeTypes: mockBulbasaur.types,
+};
+
+export const Charmander = MovesTableStory.bind({});
+Charmander.args = {
+  moves: mockCharmander.moves,
+  pokeTypes: mockCharmander.types,
 };
